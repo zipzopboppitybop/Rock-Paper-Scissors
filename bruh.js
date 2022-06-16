002d54e84f551efe91abbf5c858e9b114f68393c
Brian Washington <bw49078@gmail.com>
2022-06-16 17:17:28 -0500

    added files and added game, round, and computer functions

---
+++ bruh.js
@@ -0,0 +1,15 @@
+function playRound() {
+
+}
+
+function game() {
+    playRound()
+}
+
+const playerPick = "rock";
+const computer =  computerPlay();
+
+function computerPlay() {
+
+}
+console.log(playerPick + computer)
