bdc38de4db8b694f0479a3c540f38d7967194572
Brian Washington <bw49078@gmail.com>
2022-06-18 06:50:50 -0500

    made computer selection part of function now it changes selection every round

--- bruh.js
+++ bruh.js
@@ -1,35 +1,46 @@
-const selection = ["rock", "paper", "scissors"];
-const computerSelection = selection[Math.floor(Math.random() * selection.length)];
+
 let playerSelection = prompt();
 playerSelection = playerSelection.toLowerCase();
-const computer = computerPlay();
-
+let playerScore = 0
+let computerScore = 0
 
 function computerPlay() {
+    const selection = ["rock", "paper", "scissors"];
+    const computerSelection = selection[Math.floor(Math.random() * selection.length)];
     return computerSelection
 }
 
+
 function round() {
-    computerPlay()
+    let computerSelection = computerPlay()
     console.log("You:" + playerSelection + " vs " + "Bot:" + computerSelection);
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You Win!");
+        console.log("You Won this round!");
+        playerScore ++;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You Win!");
+        console.log("You Won this round!");
+        playerScore ++;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You Win!");
+        console.log("You Won this round!");
+        playerScore ++;
     } else if (playerSelection === computerSelection) {
         console.log("Draw");
     } else {
-        console.log("You Lose!");
+        console.log("You Lost this round!");
+        computerScore ++;
     }
-
 }  
 
 function game() {
     for (let i = 0; i < 5; i++) {
         round()
+     }
+     console.log(playerScore, computerScore);
+     if (playerScore > computerScore) {
+        console.log("You Win!");
+     } else if (playerScore < computerScore) {
+        console.log("You Lose!");
      }
 }
 
