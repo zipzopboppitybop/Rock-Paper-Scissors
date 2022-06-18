361ea83e4e7921a68bb76be75f8900d28ca0022e
Brian Washington <bw49078@gmail.com>
2022-06-18 05:27:44 -0500

    added a draw made prompt case insensitive and made console log better looking

--- bruh.js
+++ bruh.js
@@ -1,16 +1,30 @@
-function playRound() {
-
-}
-
-function game() {
-    playRound()
-}
-
-const playerPick = "rock";
-const computer =  computerPlay();
-
+const selection = ["rock", "paper", "scissors"];
+const computerSelection = selection[Math.floor(Math.random() * selection.length)];
+let playerSelection = prompt();
+playerSelection = playerSelection.toLowerCase();
+const computer = computerPlay();
 
 function computerPlay() {
-    console.log("rock")
+    return computerSelection
 }
-console.log(playerPick + computer)
+
+function round() {
+    computerPlay()
+    console.log("You:" + playerSelection + " vs " + "Bot:" + computerSelection);
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You Win!");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You Win!");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("You Win!");
+    } else if (playerSelection === computerSelection) {
+        console.log("Draw");
+    } else {
+        console.log("You Lose!");
+    }
+
+}  
+
+round()
+
