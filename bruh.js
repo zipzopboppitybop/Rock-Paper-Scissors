f36286aa4a28d53453c8e1d62b55f3be27e8c6bc
Brian Washington <bw49078@gmail.com>
2022-06-20 17:08:47 -0500

    scores, selections, and win/lose message displays on screen

--- bruh.js
+++ bruh.js
@@ -1,46 +1,54 @@
-let playerScore = 0
-let computerScore = 0
-
+//Variables
+let playerScore = 0;
+let computerScore = 0;
+let scores = 0;
+let scoreboard = document.getElementById("scores");
+let you = document.getElementById("you");
+let bot = document.getElementById("bot");
 const rock = document.getElementById("rockBtn");
 const paper = document.getElementById("paperBtn");
 const scissors = document.getElementById("scissorsBtn");
 
+//Button Selection
 rock.addEventListener("click", function() {
-    playerSelection = "rock"
-    round()
+    playerSelection = "Rock"
+    game()
 })
 paper.addEventListener("click", function() {
-    playerSelection = "paper"
-    round()
+    playerSelection = "Paper"
+    game()
 })
 scissors.addEventListener("click", function() {
-    playerSelection = "scissors"
-    round()
+    playerSelection = "Scissors"
+    game()
 })
 
+//Play Functions
 function playerPlay() {
     return playerSelection;
 }
 
 function computerPlay() {
-    const selection = ["rock", "paper", "scissors"];
+    const selection = ["Rock", "Paper", "Scissors"];
     const computerSelection = selection[Math.floor(Math.random() * selection.length)];
     return computerSelection;
 }
 
+//Actual Game
 function round() {
     let playerSelection = playerPlay();
     let computerSelection = computerPlay();
-    console.log("You:" + playerSelection + " vs " + "Bot:" + computerSelection);
+    you.textContent = "You: " + playerSelection;
+    bot.textContent = "Bot: " + computerSelection;
 
-    if (playerSelection === "rock" && computerSelection === "scissors") {
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
         console.log("You Won this round!");
         playerScore ++;
         
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         console.log("You Won this round!");
         playerScore ++;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         console.log("You Won this round!");
         playerScore ++;
     } else if (playerSelection === computerSelection) {
@@ -52,14 +60,12 @@ function round() {
 }  
 
 function game() {
-    
-    console.log(playerScore, computerScore);
-    if (playerScore > computerScore) {
-        console.log("You Win!");
-    } else if (playerScore < computerScore) {
-        console.log("You Lose!");
-    } else {
-        console.log("Draw!");
+    round()
+    scoreboard.textContent = playerScore + " - " + computerScore;
+
+    if (playerScore === 5) {
+        scoreboard.textContent = "You Win!";
+    } else if ( computerScore === 5) {
+        scoreboard.textContent = "You Lose!";
     }
 }
-
